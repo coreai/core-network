6c6b2a4760a3a1a7f74fb661db4dbd64e761b582
lolotrgeek <jon@simplyenable.com>
2021-10-28 10:02:56 -0500

    fix: parameterize & integrate cote on build

--- src/core.js
+++ src/core.js
@@ -1,10 +1,8 @@
-const { Publisher, Publish } = require('./publisher')
-const { Subscriber } = require('./subscriber')
 const { Validator } = require('./validator')
 const { Parser } = require('./parser')
 const { log } = require('./utils')
 const defaults = require('./parameters/defaults.json')
-const branchy = require('branchy')
+const { Subscriber, Publisher } = require('cote')
 
 
 
@@ -13,56 +11,29 @@ function Core(functions, parameters) {
 }
 
 function Build(functions, parameters) {
-    let state = State(parameters)
     if (parameters.generator && parameters.generator !== 'false') {
-        let options = Options(parameters, state, 'publisher')
-        let publisher = Publisher(options)
-        state = State(parameters, state, { publisher })
-        if (typeof parameters.generator === 'number') setInterval(() => Publish(options, functions()), parameters.generator)
-        else Publish(options, functions())
+        const publisher = new Publisher({...parameters, name: parameters.name + "_publisher"})
+        if (typeof parameters.generator === 'number') setInterval(() => publisher.publish(parameters.broadcasts[0], functions()), parameters.generator)
+        else publisher.publish(parameters.broadcasts[0], functions())
     }
     else {
-        let publisher = Publisher(Options(parameters, state, 'publisher'))
-        state = State(parameters, state, { publisher })
-        let subscriber = Subscriber(Options(parameters, state, 'subscriber'), data => {
-            log(parameters, "Processing", data)
-            let valid_data = Validator(parameters, Parser(parameters, data))
-            if (valid_data !== false) {
-                let output = functions(valid_data)
-                Publish(Options(parameters, state, 'publisher'), output)
-            }
+        const publisher = new Publisher({...parameters, name: parameters.name + "_publisher"})
+        const subscriber = new Subscriber({...parameters, name: parameters.name + "_subscriber"})
+        parameters.subscribesTo.forEach(channel => {
+            subscriber.on(channel, data => {
+                log(parameters, "Processing", data)
+                let valid_data = Validator(parameters, Parser(parameters, data))
+                if (valid_data !== false) {
+                    let output = functions(valid_data)
+                    publisher.publish(parameters.broadcasts[0], output)
+                }
+            })
         })
-        state = State(parameters, state, { subscriber })
-    }
-    console.log('STATE:', state)
 
+    }
     // log(parameters, "Build", state)
 }
 
-function State(parameters, state, { publisher, subscriber } = {}) {
-    if (!state) state = {}
-    if (!state[parameters.name]) {
-        state[parameters.name] = {}
-        Object.assign(state[parameters.name], parameters)
-    }
-    if (publisher) {
-        state[parameters.name].publisher = publisher
-    }
-
-    if (subscriber) {
-        state[parameters.name].subscriber = subscriber
-    }
-    return state
-}
-
-function Options(parameters, state, type) {
-    try {
-        state[parameters.name].name = parameters.name + '_' + type
-        return state[parameters.name]
-    } catch (error) {
-        log(parameters, `No options for ${parameters.name} in state -->`, state)
-    }
-}
 
 function Parameterize(parameters) {
     if (!parameters) parameters = {}
