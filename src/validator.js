c3c279f9455c88a19fff59384daabd1471bafa20
lolotrgeek <jon@simplyenable.com>
2021-11-12 12:10:34 -0600

    fix: remove output validation

--- src/validator.js
+++ src/validator.js
@@ -7,11 +7,32 @@ const { log } = require('./utils')
  */
 function Validator(parameters, data) {
     log(parameters, "Validating", data)
-    if (typeof data === 'null' || data === null) { log(parameters, "Data inValid --> null", data); return false }
-    if (typeof data === 'undefined'|| data === undefined) { log(parameters, "Data inValid  --> undefined", data); return false }
-    if (typeof data === 'number' && isNaN(data)) { log(parameters, "Data inValid --> NaN", data); return false }
+    let dataValid = validateData(data)
+    if(dataValid === false) { log(parameters, "Data inValid --> data malformed", data); return false }
+    let heardSelf = hearSelf(parameters, data)
+    if (heardSelf === true) { log(parameters, "Data inValid --> data from self", data); return false }
     log(parameters, "Data Valid", data)
-    return data
+    let output = validateOutput(parameters, data.output)
+    return output
+}
+
+function hearSelf(parameters, data) {
+    if (typeof data === 'object' && data.sender && data.sender === parameters.name) return true
+}
+
+function validateData(data) {
+    if (typeof data !== 'object') return false
+    if (typeof data.sender !== 'string') return false
+    return true
+}
+
+function validateOutput(parameters, output) {
+    if (typeof output === 'null' || output === null) { log(parameters, "output inValid --> null", output); return false }
+    if (typeof output === 'undefined' || output === undefined) { log(parameters, "output inValid  --> undefined", output); return false }
+    if (typeof output === 'number' && isNaN(output)) { log(parameters, "output inValid --> NaN", output); return false }
+    log(parameters, "Output Valid", output)
+    return output
+
 }
 
 module.exports = { Validator }
